17e1c59792c7de81c0bb783ff5e4bb5bf6294c25
Rucha Harshe <77910528+Rucha2000@users.noreply.github.com>
2022-08-02 16:51:52 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-    public static void main(String s) {
+    public static void main(String [] args) {
         System.out.println("Hello All");
         for(int i=0; i<20; i++) {
             System.out.println("Value of I : "+i);
